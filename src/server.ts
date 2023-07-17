223a47c935e95f9bc541c4c0920dc8b4cae9d76b
Ravi Singh <ravi.u.singh@here.com>
2023-07-17 12:17:27 +0530

    resolved cors issue while socket connetion with client

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,21 @@
-const cors = require('cors');
-// import cors from 'cors';
-import express from 'express';
-import http from 'http';
+const cors = require("cors");
+import express from "express";
+import http from "http";
 import socketHandler from "./socketHandler";
 
 const app = express();
 const server = http.createServer(app);
 
-// Enable CORS middleware
-app.use(cors({ origin: "*"}));
-
 // Set up server port
 const port = process.env.PORT || 3000;
 
 // Define a route
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
 });
+
+// Enable CORS middleware
+app.use(cors());
 
 //setup socket connection
 socketHandler(server);
--- src/socketHandler.ts
+++ src/socketHandler.ts
@@ -1,22 +1,27 @@
-import { Server, Socket } from 'socket.io';
-import { Server as HttpServer } from 'http';
+import { Server, Socket } from "socket.io";
+import { Server as HttpServer } from "http";
 
 // Export a function that sets up Socket.IO connections
 const socketSetup = (server: HttpServer): void => {
-    const io = new Server(server);
-  
-    io.on('connection', (socket: Socket) => {
-      console.log('A user connected');
-  
-      socket.on('cardSelection', (data) => {
-        console.log('Card selected >>>>>>>', data);
-        io.emit('cardSelection', data);
-      });
-  
-      socket.on('disconnect', () => {
-        console.log('A user disconnected');
-      });
-    });
-  }
+  const io = new Server(server);
 
- export default socketSetup;
+  //This resolve the cors issue.
+  io.engine.on("headers", (headers, req) => {
+    headers["Access-Control-Allow-Origin"] = "*"; // url to all
+  });
+
+  io.on("connection", (socket: Socket) => {
+    console.log("A user connected");
+
+    socket.on("cardSelection", (data) => {
+      console.log("Card selected >>>>>>>", data);
+      io.emit("cardSelection", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected");
+    });
+  });
+};
+
+export default socketSetup;
